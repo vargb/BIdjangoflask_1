e85051cdb362d1b3c1e27694dbb1abd94eeae8c1
VGB <9965vgb@gmail.com>
2023-11-25 17:18:15 +0530

    iwannakillmyself

--- djangoServer/djangoServer/models.py
+++ djangoServer/djangoServer/models.py
@@ -1,6 +1,59 @@
 import pymongo
 from djangoServer2.config.config import conf
 
-client = pymongo.MongoClient("mongodb://"+conf.mongodb.host+":"+conf.mongodb.port)
-dbname=client.get_database(conf.mongodb.dbname)
-collection=dbname.get_collection(conf.mongodb.collection)
+
+schema={
+    "$jsonSchema":{
+        "bsonType":"object",
+        "required":["product_Id"],
+        "properties":{
+            "product_Id":{
+                "bsonType":"int"
+            },
+            "category":{
+                "bsonType":"string"
+            },
+            "industry":{
+                "bsonType":"string"
+            },
+            "business_scale":{
+                "bsonType":"string"
+            },
+            "user_type":{
+                "bsonType":"string"
+            },
+            "no_of_users":{
+                "bsonType":"string"
+            },
+            "deployment":{
+                "bsonType":"string"
+            },
+            "os":{
+                "bsonType":"string"
+            },
+            "mobile_apps":{
+                "bsonType":"string"
+            },
+            "pricing":{
+                "bsonType":"string"
+            },
+            "rating":{
+                "bsonType":"string"
+            },
+            "id":{
+                "bsonType":"string"
+            }
+        }
+    }
+}
+
+
+def InitMongoDB():
+    client = pymongo.MongoClient("mongodb://"+conf.mongodb.host+":"+conf.mongodb.port)
+    dbname=client.get_database(conf.mongodb.dbname)
+    if conf.mongodb.collection in dbname.list_collection_names():
+        collection=dbname.get_collection(conf.mongodb.collection)
+    else:
+        collection=dbname.create_collection(conf.mongodb.collection,validator=schema)
+    return collection
+    
--- djangoServer/djangoServer/views.py
+++ djangoServer/djangoServer/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from .models import collection
+from django.http import HttpResponse,JsonResponse
+from .models import InitMongoDB
 
-def sayHello(request):
-    return HttpResponse(collection.count_documents({}))
+coll=InitMongoDB()
+
